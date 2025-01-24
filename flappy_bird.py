841629e0669b2e933955f97afdf19ca0a1e8184a
Crague Ilian <ilian.crague@etu.u-paris.com>
2025-01-24 17:31:44 +0100

    flappy biiiiiiiiird

--- flappy_bird.py
+++ flappy_bird.py
@@ -20,6 +20,10 @@ WIDTH_PIPE = 2
 X_PIPE = LENGTH
 Y_PIPE = 15
 SCORE = 0
+FINISHED = False
+N = 0
+weights = []
+LENGTH_TAB = 2
 
 # Variables globales
 STARTED = False
@@ -29,12 +33,14 @@ current_weight = 0  # Initialisation de current_weight
 # Fonction de mise à jour du graphique
 def update_graph(ax, canvas):
     """Mise à jour du graphique."""
-    global current_weight
+    global current_weight, POS_BIRD
 
     ax.clear()
 
     # Affiche le poids mesuré
-    ax.scatter(POS_BIRD, current_weight, color='red', s=100, label=f'Point ({POS_BIRD}, {current_weight})')
+    h = MAX_WEIGHT if current_weight > MAX_WEIGHT else current_weight
+    
+    ax.plot([POS_BIRD - i for i in range(LENGTH_TAB)], weights, color="red", marker='')
 
     # Ajuste l'échelle des axes
     ax.set_xlim(0, LENGTH)
@@ -50,39 +56,63 @@ def update_graph(ax, canvas):
 
     canvas.draw()
 
+def check_dead():
+    global current_weight, X_PIPE, Y_PIPE, pipes
+    for pipe in pipes:
+        if POS_BIRD >= pipe[0] and POS_BIRD <= pipe[0] + WIDTH_PIPE: #same x
+            if pipe[2]:
+                if current_weight >= pipe[1]:
+                    return True
+            else :
+                if current_weight <= pipe[1]:
+                    return True
+    return False
+
 # Callback Bluetooth pour récupérer le poids
 def advertisement_callback(device, advertisement_data):
     """Gère les données de publicité Bluetooth."""
-    global current_weight, STARTED, Y_PIPE, X_PIPE, pipes, SCORE
-
+    global current_weight, STARTED, Y_PIPE, X_PIPE, pipes, SCORE, FINISHED, N
+    
     if device.address == "2A:C0:19:11:23:B7":
         if advertisement_data.manufacturer_data:
             for manufacturer_id, data in advertisement_data.manufacturer_data.items():
                 if len(data) > max(WEIGHT_OFFSET + 1, STABLE_OFFSET):
+                    if N < 25:
+                        N+=1
+                
                     weight = (data[WEIGHT_OFFSET] & 0xff) << 8 | (data[WEIGHT_OFFSET + 1] & 0xff)
                     current_weight = weight / 100
+                    weights.append(current_weight)
+                    if len(weights) > LENGTH_TAB:
+                        weights.pop(0)
 
-                    # Affiche le poids
+
                     weight_display.config(text=f"{current_weight:.2f} kg \n\n Score : {SCORE} pts")
 
 
-                    if len(pipes) == 0 or pipes[-1][0] < LENGTH - 7:  # Ajoute un tuyau si l'écran a de la place
+                    if not FINISHED and (len(pipes) == 0 or pipes[-1][0] < LENGTH - 7):  # Ajoute un tuyau si l'écran a de la place
                         Y_PIPE = random.randint(MIN_Y + (MAX_Y - MIN_Y) // 4, MAX_Y - (MAX_Y - MIN_Y) // 4)
                         X_PIPE = LENGTH
-                        pipes.append([X_PIPE, Y_PIPE, random.choice([1, 0])])
+                        if N > 20:
+                            pipes.append([X_PIPE, Y_PIPE, random.choice([1, 0])])
 
-                    # Déplace tous les tuyaux
+
                     for pipe in pipes:
-                        pipe[0] -= 1  # Déplacer chaque tuyau à gauche
+                        pipe[0] -= 1
 
-                    # Supprimer les tuyaux sortis de l'écran
-                    if pipes[0][0] < 0:
+
+                    if len(pipes) > 0 and pipes[0][0] < 0:
                         SCORE += 1
                         pipes = pipes[1:]
 
+                    if check_dead():
+                        FINISHED = True
+                        pipes = []
+
                     update_graph(ax, canvas)
 
-# Fonction asynchrone de balayage Bluetooth
+
+
 async def scan_for_advertisements():
     """Recherche les publicités Bluetooth."""
     scanner = BleakScanner()
